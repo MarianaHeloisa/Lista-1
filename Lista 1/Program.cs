2856355cfb6145b69bb9cfc43d7962e6a80c3e1b
MarianaHeloisa <mariana.heloisa@aluno.ifsp.edu.br>
2023-03-08 11:17:24 -0300

    Adicionar arquivos de projeto.

---
+++ Lista 1/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercicio 1
+
+
+            //int metros;
+            //Console.Write("digite u numero de metros: ");
+            //metros = int.Parse(Console.ReadLine());
+            //cm = metros * 100;
+            //mm = metros * 1000;
+
+            //Console.Write("a quantidade em centimetros é: " + cm);
+            //Console.Write("a quantidade em milimetros é: " + mm);
+
+            //Console.ReadKey();
+
+            //exercicio 2
+
+            //int fa1, resInt;
+            //float fa2, resFloat;
+
+            //Console.Write("digite um valor em fahrenheit int: ");
+            //fa1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("digite um valor em fahrenheit float: ");
+            //fa2 = float.Parse(Console.ReadLine());
+
+            //resInt = (fa1 -32 * 0) * (5 / 9);
+            //resFloat = (fa2 - 32f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("o valor é: " + resFloat);
+            //Console.WriteLine("resultado int: " + resInt);
+
+            //Console.ReadKey();
+
+
+            //exercicio 3
+
+            //float peso, altura, resF;
+
+            //Console.Write("digite seu peso: ");
+            //peso = float.Parse(Console.ReadLine());
+
+            //Console.Write("digite sua altura: ");
+            //altura = float.Parse(Console.ReadLine());
+
+
+            //resF = peso / altura * altura;
+
+            //Console.WriteLine(" o resultado é: " + resF );
+            //Console.ReadKey();  
+
+            //exercicio 4 
+
+            // int num1, num2, num3, peso1, peso2, peso3, resultado;
+
+            //Console.Write("digite numero 1 : ");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write(" digite numero 2 : ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //Console.Write(" digite numero 3: ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //Console.Write(" digite peso 1: ");
+            //peso1 = int.Parse(Console.ReadLine());
+
+            //Console.Write(" digite peso 2: ");
+            //peso2 = int.Parse(Console.ReadLine());
+
+            //Console.Write(" digite peso 3: ");
+            //peso3 = int.Parse(Console.ReadLine());  
+
+            //resultado = num1 + num2 + num3 + peso1 + peso2 + peso3;
+
+            // Console.WriteLine(" o valor da media é: " + resultado);
+            //Console.ReadKey();
+
+            // exercicio 5
+
+            //float distancia, tempo, velocidade;
+
+            //Console.Write("qual a distancia?: ");
+            //distancia = float.Parse(Console.ReadLine());
+
+            //Console.Write("qual o tempo?: ");
+            //tempo = float.Parse(Console.ReadLine());
+
+            //velocidade = distancia / tempo;
+
+            //Console.WriteLine(" a velocidade é: " + velocidade);
+            //Console.ReadLine();
+
+            // exercicio 6
+
+            //float salario, resultado;
+
+            //Console.Write("o seu salario é?: ");
+            //salario = float.Parse(Console.ReadLine());
+
+            //resultado = salario * 25 / 100 + salario;
+
+            //Console.WriteLine("o seu salario foi para: " + resultado);
+            //Console.ReadKey();
+
+            // exercicio 7 
+
+            //float dolar, real, resultado;
+
+            //Console.Write("qual o valor em reais?: " );
+            //real = float.Parse( Console.ReadLine() );
+
+            //Console.Write("qual a cotação do dolar?: ");
+            //dolar = float.Parse( Console.ReadLine() );
+
+            // resultado = real / dolar;
+
+            //Console.WriteLine(" o valor em dolares é: " + resultado);
+            //Console.ReadKey();
+
+            // exercicio 8 
+
+            //int num1, resultado1, resultado2, resultado3;
+
+            //Console.Write("qual o numero?: ");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //resultado1 = (num1 * 3) + 1;
+            //resultado2 = (num1 * 2) - 1;
+            //resultado3 = resultado1 + resultado2;
+
+            //Console.WriteLine("o valor é: " + resultado3);
+            //Console.ReadKey();
+
+            //exercicio 9
+
+            //int num, milhar, centena, dezena, unidade;
+
+            //Console.Write("digite um numero de 4 digitos: ");
+            //num = int.Parse(Console.ReadLine());
+
+            // separa cada digito do numero e imprime em uma linha separada
+
+            //milhar = num / 1000;
+            //centena = num / 100) % 10;
+            //dezena = (num / 10 % 10;
+            //unidade = num % 10;
+
+            //Console.WriteLine(milhar);
+            //Console.WriteLine(centena);
+            //Console.WriteLine(dezena);
+            //Console.WriteLine(unidade);
+            //Console.ReadKey();
+
+            //exercicio 10 
+
+            //float investir = 1000, d1, d2, d3;
+
+            //investir = investir - ((investir * 3.4f) / 100);
+            //investir = investir + ((investir * 2.8f) / 100);
+            //investir = investir + ((investir * 14f) / 100);
+            //investir = investir - ((investir * 8.5) / 100);
+
+            //Console.WriteLine(investir);
+            //Console.ReadKey();
+
+            // exercicio 11
+
+            //int n, cubo, quadrado;
+
+            //Console.Write("coloque um numero: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //quadrado = n * n;
+            //cubo = n * n * n;
+
+            //Console.WriteLine(" o numero ao quadrado é: " + quadrado);
+            //Console.WriteLine(" o numero ao cubo é: " + cubo);
+
+            //Console.ReadKey();
+
+            //   exercicio 12
+
+            //float h, pesoH, pesoM
+
+            //Console.Write("qual a sua altura?: ");
+            //h = float.Parse(Console.ReadLine());
+
+            //   pesoH = (72.7f * h) - 58f;
+            //   pesoM = (62.1f * h) - 44.7f;
+
+            //   Console.WriteLine(" o peso ideal para um homem é: " + pesoH);
+            //   Console.WriteLine(" o peso ideal para uma mulher é: " + pesoM);
+
+            //   Console.ReadKey();
+        }
+    }
+}
